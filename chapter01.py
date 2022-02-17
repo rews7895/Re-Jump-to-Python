950cff5a04840235374e10f753fdc51ddaf7453a
Junseok <yalvz@naver.com>
2022-02-17 22:03:24 +0900

    01. 파이썬이란 무언인가?
    간단한 문법들 써보기

---
+++ chapter01.py
@@ -0,0 +1,31 @@
+print(1 + 2)
+
+a = 3
+b = 2.4
+print(a / b)
+
+print(a * b)
+
+str = "Python"
+
+print(str)
+
+if a > 1:
+  print('크다')
+
+for innerItem in [1, 2, 3]:
+  print(innerItem)
+
+i = 0
+while i < 3:
+  i = i + 1
+  print(i)
+
+def add(a, b):
+  return a + b
+
+returnValue = add(1, 4)
+print(returnValue)
+
+# Hello World는 국룰~!
+print("Hello World!")
