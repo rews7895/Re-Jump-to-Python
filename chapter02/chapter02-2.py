79365983e5ea3665ca1eb32d2d0a56049df8c158
Junseok <yalvz@naver.com>
2022-02-24 22:13:36 +0900

    chapter02 연습문제 풀이

--- chapter02/chapter02-2.py
+++ chapter02/chapter02-2.py
@@ -6,135 +6,135 @@ c = """Life is too short, You need python"""
 d = '''Life is too short, You need python'''
 e = "Python\'s fun"
 
-print(e)
+# print(e)
 
 f = "\"Python is very easy.\" he says."
-print(f)
+# print(f)
 
 g = "aaa\nbbb"
-print(g)
+# print(g)
 
 multiline = '''
   Life is to short
   You need python
 '''
 
-print(multiline)
+# print(multiline)
 
 head = "Python"
 tail = " is fun!"
 
-print(head + tail)
+# print(head + tail)
 
 string = 'Python'
-print(string * 2)
+# print(string * 2)
 
-print("=" * 50)
-print("My Program")
-print("=" * 50)
+# print("=" * 50)
+# print("My Program")
+# print("=" * 50)
 
 txt = "Life is too short"
-print(len(txt))
+# print(len(txt))
 
 date = "2022-02-20 18:23:59"
 
-print(date[4])
-print(date[0:10])
-print(date[0] + date[1] + date[2] + date[3])
-print(date[:10])
-print(date[11:])
+# print(date[4])
+# print(date[0:10])
+# print(date[0] + date[1] + date[2] + date[3])
+# print(date[:10])
+# print(date[11:])
 
 txt = "치킨 가격은 %d원 입니다." %19000
-print(txt)
+# print(txt)
 
 txt = "배달 시킨 치킨은 %s입니다." %"BBQ"
-print(txt)
+# print(txt)
 
 cEa = 3
 txt = "치킨은 총 %d마리 배달시켰습니다." %cEa
-print(txt)
+# print(txt)
 
 txt = "치킨을 총 %d%%먹었습니다." %90
-print(txt)
+# print(txt)
 
 txt = "%0.2f" %3.141592
-print(txt)
+# print(txt)
 
 txt = "I eat {0} apples".format(3)
-print(txt)
+# print(txt)
 
 number = 3
 txt = "I eat {0} apples".format(number)
-print(txt)
+# print(txt)
 
 number = 10
 day = "three"
 txt = "i ate {0} apples. so I was sick for {1} days.".format(number, day)
-print(txt)
+# print(txt)
 
 txt = "{0:<10}".format("hi")
-print(txt + 'a')
+# print(txt + 'a')
 
 txt = "{0:>10}".format("hi")
-print(txt)
+# print(txt)
 
 #가운데 정렬
 txt = "{0:^10}".format("hi")
-print(txt)
+# print(txt)
 
 txt = "{0:=^10}".format("Hi")
-print(txt)
+# print(txt)
 
 y = 3.42134234
 txt = "{0:0.4f}".format(y)
-print(txt)
+# print(txt)
 
 #문자열 포멧팅
 name = "홍길동"
 age = 31
 txt = f'나의 이름은 {name}입니다. 나이는 {age}입니다.'
-print(txt)
+# print(txt)
 
 age = 30
 txt = f'나는 내년이면 {age + 1}살이 된다.'
-print(txt)
+# print(txt)
 
 txt = f'{"hi":=^10}'
-print(txt)
+# print(txt)
 
 txt = f'{"python":!^12}'
-print(txt)
+# print(txt)
 
 #문자 개수 세기
 a = "apple"
-print(a.count('p'))
-print(len(a))
+# print(a.count('p'))
+# print(len(a))
 
 #위치
 a = "Python is the best choice"
-print(a.find('b'))
-print(a.find('k'))
+# print(a.find('b'))
+# print(a.find('k'))
 
-print(",".join('abcd'))
+# print(",".join('abcd'))
 
 a = "Life is too short"
-print(a.index('t'))
-# print(a.index('k'))
+# print(a.index('t'))
+# # print(a.index('k'))
 
 a = "apple"
-print(a.upper())
+# print(a.upper())
 
 a = "HI"
-print(a.lower())
+# print(a.lower())
 
 a = " hi "
-print(a.lstrip())
-print(a.rstrip())
-print(a.strip())
+# print(a.lstrip())
+# print(a.rstrip())
+# print(a.strip())
 
 a = "Life is too short"
-print(a.replace("Life", "Your Leg"))
-print(a.split())
+# print(a.replace("Life", "Your Leg"))
+# print(a.split())
 
 b = "a:b:c:d"
-print(b.split(":"))
+# print(b.split(":"))
--- chapter02/chapter02-6.py
+++ chapter02/chapter02-6.py
@@ -5,42 +5,42 @@
 #     2) 순서가 없다.
 
 s1 = set([1, 2, 3])
-print(s1)
+# print(s1)
 
 s2 = set('hello')
-print(s2)
+# print(s2)
 
 # 중복을 제거하기 위한 필터 역할로 종종 사용되기도 한다.
 s1 = list(set([1, 4, 3, 2, 2]))
 s1.sort()
-print(s1)
+# print(s1)
 
 s1 = set([1, 2, 3, 4, 5, 6])
 s2 = set([4, 5, 6, 7, 8, 9])
 
 # 교집합
-print(s1 & s2)
-print(s1.intersection(s2))
+# print(s1 & s2)
+# print(s1.intersection(s2))
 
 # 합집합
-print(s1 | s2)
-print(s1.union(s2))
+# print(s1 | s2)
+# print(s1.union(s2))
 
 # 차집합
-print(s1 - s2)
-print(s2 - s1)
+# print(s1 - s2)
+# print(s2 - s1)
 
 # 1개의 값 추가
 s1 = set([1, 2, 3])
 s1.add(4)
 s1.add(2)
-print(s1)
+# print(s1)
 
 # 여러개의 값 추가
 s1.update([4, 5, 6, 7])
-print(s1)
+# print(s1)
 
 # 특정값 제거(1개만 가능)
 s1.remove(2)
-print(s1)
+# print(s1)
 
