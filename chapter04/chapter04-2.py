08d34841c11a73729bb3d46d25adb3e1ceb2203b
Junseok <yalvz@naver.com>
2022-02-26 20:46:48 +0900

    04-2 사용자 입력과 출력
    input()과 print()의 사용

---
+++ chapter04/chapter04-2.py
@@ -0,0 +1,15 @@
+"""
+04-2. 사용자 입력과 출력
+"""
+# a = input()
+# print(a)
+
+# number = input('숫자를 입력해주세요: ')
+# print(number)
+
+print('Life' 'is' 'too short')
+print('Life' + 'is' + 'too short')
+
+for i in range(10):
+  print(i, end = '  ')
+  print('')
