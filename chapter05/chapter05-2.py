fc70398e4ef7be4006bd8ee5034dbc98139bd4c1
Junseok <yalvz@naver.com>
2022-02-27 19:12:29 +0900

    05-2 모듈

---
+++ chapter05/chapter05-2.py
@@ -0,0 +1,17 @@
+"""
+05-2. 모듈
+  - from 모듈이름 import 모듈함수
+"""
+from modules.mod1 import *
+from modules.mod2 import *
+
+# print(add(3, 4))
+# print(sub(4, 2))
+
+# print(PI)
+# a = Math()
+# print(a.solv(2))
+# print(add(PI, 4.4))
+
+# result = add(3, 4)
+# print(result)
---
+++ chapter05/modules/mod1.py
@@ -0,0 +1,10 @@
+def add(a, b):
+  return a + b
+
+def sub(a, b):
+  return a - b
+
+# 파일을 직접 실행할 때만 해당 if문을 수행
+if __name__ == '__main__':
+  print(add(1, 4))
+  print(sub(4, 2))
